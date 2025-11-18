99c5f6d1f148f203a7b821f58db251e91210e3e8
MoisesEastland1 <meastland977@gmail.com>
2025-11-18 15:59:29 -0600

    adding js to repo

---
+++ script.js
@@ -0,0 +1,23 @@
+let latRes = document.querySelector('lat');
+let lgnRes = document.querySelector('lgn');
+const map = L.map('map').setView([0, 0], 2);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+const marker = L.marker([0, 0]).addTo(map);
+
+navigator.geolocation.getCurrentPosition(function (pos) {
+const lat = pos.coords.latitude;
+const lng = pos.coords.longitude;
+
+
+
+console.log(`latitude ${lat}`);
+console.log(`longitude ${lng}`);
+marker.setLatLng([lat, lng]).update();
+map.setView([lat, lng], 13);
+
+marker.bindPopup('<strong>Hello World</strong> <br> This is my location ');
+});
